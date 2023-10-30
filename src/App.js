ade099f92482d96e3e33ae1928f7f7fc5eb609a3
tianahR <43481097+tianahR@users.noreply.github.com>
2023-10-29 22:01:18 -0400

    Lifting State And Props Handling

--- src/App.js
+++ src/App.js
@@ -8,20 +8,32 @@ function App() {
 
     
   //a new state variable named newTodo with update function named setNewTodo
-  const [newTodo, setNewTodo] = React.useState("");
+  //const [newTodo, setNewTodo] = React.useState("");
+
+  //Create new state variable named todoList with setter setTodoList and default value of an empty Array
+  const [todoList, setTodoList] = React.useState([]);
+
+  
+  // Declare a new function named addTodo that takes newTodo as a parameter
+  //  Call the setTodoList state setter and use the spread operator to pass the existing Objects in the todoList Array along with the newTodo Object
+  const addTodo = (newTodo)=> {
+    setTodoList([...todoList, newTodo]);
+  }
 
 
   // callback handler prop named onAddTodo
-    const onAddTodo = (event) => {
-      setNewTodo(event.target.title.value);
-      };
+  //  const onAddTodo = (event) => {
+  //    setNewTodo(event.target.title.value);
+  //    };
      return (
       
       <div>
             <h1> Todo List </h1>
-            <AddTodoForm  onAdd={onAddTodo}/>
-            <p> Value of newTodo is <strong>{newTodo}</strong></p>
-            <TodoList/>
+            <AddTodoForm  onAddTodo={addTodo}/>
+            {/* <p> Value of newTodo is <strong>{newTodo}</strong></p> */}
+            {/* <TodoList/> */}
+            {/* <AddTodoForm onAddTodo={addTodo} /> */}
+            <TodoList todoList={todoList} />
          
        </div>
        
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,31 +1,14 @@
 import React from "react" ;
 import TodoListItem from "./TodoListItem";
 
-
-//array of objects to do 
-const todoList = [
-    {
-      id: 1,
-      title: 'Read lessons',
-    },
-    {
-      id: 2,
-      title: 'Do exercises',
-    },
-    {
-      id: 3,
-      title: 'Complete assignments',
-    }
-  
-    ];
-
-
-const  TodoList = () =>
+const  TodoList = ({todoList}) =>
     (
         <>
             <ul>
                 {todoList.map(function (item) {
-                    return <TodoListItem key={item.id} item={item}/>;
+                    
+                    // return <TodoListItem key={item.id} todo ={item}/>;
+                    return <TodoListItem key={item.id} todo ={item.title}/>;
 
             })}
          
